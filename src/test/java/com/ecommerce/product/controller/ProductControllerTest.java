793e91be456f28a903835f869b714c4c83631e02
Mete <burakmetecanpinar@gmail.com>
2026-03-15 18:29:51 +0300

    test: Implement unit tests for ProductController covering product creation, retrieval, stock updates, and validation.

--- src/test/java/com/ecommerce/product/controller/ProductControllerTest.java
+++ src/test/java/com/ecommerce/product/controller/ProductControllerTest.java
@@ -4,10 +4,10 @@ import com.ecommerce.product.dto.*;
 import com.ecommerce.product.exception.InsufficientStockException;
 import com.ecommerce.product.exception.ProductNotFoundException;
 import com.ecommerce.product.service.ProductService;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -28,8 +28,7 @@ class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
 
     @MockitoBean
     private ProductService productService;
